5b5d70d5506efa4dbe6c411c2dce733044c5ef85
diegoholiveirav <diegoholiveirav@gmail.com>
2021-03-13 10:31:26 -0300

    Adicionando README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+#Go Barber Web
+
+![Imagem de Entrada]('./../go-barber-web-login.png)
+
+**Feito com muito carinho e dedicação 💙**
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Massaroca</h1>;
+const Dashboard: React.FC = () => <h1>Aqui fica a Dashboard</h1>;
 
 export default Dashboard;
